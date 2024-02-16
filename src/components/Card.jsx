ca8b16dedeac46fa16aee072301d3d677a0ff97f
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2024-02-16 11:01:09 +0600

    add range slider

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -25,33 +25,33 @@ const Card = (product) => {
   };
 
   return (
-    <div className="w-64 border border-gray-400 rounded-md hover:shadow-md cursor-pointer">
-      <div className="w-full relative bg-white rounded-t-md p-4">
-        <div className="absolute top-2 right-2 flex flex-row items-center justify-center h-8 p-2 bg-orange-500 rounded-full">
+    <div className="flex w-80 border border-gray-400 rounded-md hover:shadow-md cursor-pointer">
+      <div className="w-full relative bg-white rounded-md p-4">
+        {/* <div className="absolute top-2 right-2 flex flex-row items-center justify-center h-8 p-2 bg-orange-500 rounded-full">
           <p className="text-sm font-semibold text-white">$ {product.price}</p>
-        </div>
+        </div> */}
         <img
           src={product.images[0]}
           alt={product.title}
           className="w-full h-44 rounded-t-md object-contain"
         />
       </div>
-      <div className="w-full h-32 rounded-b-md flex flex-col border-t border-gray-400 bg-[#EFEFEF]">
-        <div className="flex flex-row w-full pt-1 px-2">
-          <div className="flex items-center">
-            {renderRatingStars()}
-            <p className="text-sm font-semibold ml-2">{product.rating}</p>
-          </div>
-        </div>
-        <div className="flex flex-col flex-grow items-center justify-between ">
-          <div className="flex flex-row  w-full pt-1 px-2">
+      <div className="w-full rounded-b-md flex flex-col bg-[#EFEFEF]">
+        <p className="w-full font-bold  py-1 text-left px-2 overflow-hidden">
+            <p className="line-clamp-3">$ {product.price}</p>
+        </p>
+        <div className="flex flex-col flex-grow items-center justify-between pb-4">
+          <div className="w-full pt-1 px-2">
             <p className="text-sm font-semibold line-clamp-3">
               {product.title}
             </p>
+            <p className="w-full text-xs font-semibold  py-1  text-[#8A8888] text-justify overflow-hidden">
+              <p className="line-clamp-3">{product.description}</p>
+            </p>
           </div>
-          <p className="flex flex-row w-full text-xs font-semibold px-2 py-1  text-[#8A8888] text-justify overflow-hidden">
-            <p className="line-clamp-2">{product.description}</p>
-          </p>
+          <div className="flex flex-row items-center justify-center h-8 p-4 bg-orange-500 rounded-full">
+            <p className="text-sm font-semibold text-white">Add to cart</p>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import Card from '../components/Card';
+import RangeSlider from '../components/RangeSlider';
 
 export default function Home() {
   const [products, setProducts] = useState([]);
@@ -26,19 +27,32 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="flex flex-col items-center md:justify-center w-full md:w-3/4 p-6 md:pl-2 md:pr-5 md:py-5 bg-[#FCFBFC] rounded-md">
-      <div className="flex flex-col flex-grow justify-start w-full bg-white border border-gray-500 shadow-sm rounded-md">
-        {error ? (
-          <div className="flex flex-col items-center justify-center w-full h-full">
-            <p className="text-2xl font-semibold">Error fetching data 😔</p>
+    <div className="flex flex-grow w-full lg:flex-row flex-col">
+      <div className="flex flex-col items-center w-full lg:w-1/4 p-6 h-72 lg:h-80 lg:pl-6 lg:pr-2 py-5 bg-[#FCFBFC] rounder-lg ">
+        <div className="flex flex-col flex-grow items-center justify-center w-full h-1/4 bg-white border border-gray-500 shadow-sm rounded-lg">
+          <div className="flex flex-col  w-full justify-start px-5 text-xl pt-3 font-semibold flex-grow">
+            <p className="w-full ">Filters</p>
+            <div className="flex w-full items-center justify-between pt-4">
+              <p className="w-full text-sm ">Category</p>
+            </div>
+            <RangeSlider/>
           </div>
-        ) : (
-          <div className="flex flex-row flex-wrap gap-4 w-full justify-start px-5 py-4">
-            {products.map((product) => (
-              <Card key={product.id} product={product}/>
-            ))}
-          </div>
-        )}
+        </div>
+      </div>
+      <div className="flex flex-col items-center lg:justify-center w-full lg:w-3/4 p-6 lg:pl-2 lg:pr-5 lg:py-5 bg-[#FCFBFC] rounded-lg">
+        <div className="flex flex-col flex-grow justify-start w-full bg-white border border-gray-500 shadow-sm rounded-lg">
+          {error ? (
+            <div className="flex flex-col items-center justify-center w-full h-full">
+              <p className="text-2xl font-semibold">Error fetching data 😔</p>
+            </div>
+          ) : (
+            <div className="flex flex-row flex-wrap gap-4 w-full justify-center px-5 py-4">
+              {products.map((product) => (
+                <Card key={product.id} product={product}/>
+              ))}
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
